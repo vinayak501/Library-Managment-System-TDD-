2a25af83d1e79fb484e332640bb8bb40c83c62cd
vinayak501 <vinayakchavda2004@gmail.com>
2024-08-31 19:37:38 +0530

    Updated Tests

--- index.test.js
+++ index.test.js
@@ -3,21 +3,32 @@ import Library from './index';
 
 describe('Library Managment Sytem',()=>{
     let library;
+
+    // beforeEach function of vitest will reInitialize library object before each test case 
     beforeEach(() => {
       library = new Library();
     });
-    it("should add book whenever we run addBook", () => {
+
+
+    // Test case to check **AddBook** Function
+    it("should add book whenever user add Book", () => {
       expect(library.addBook("1234", "Ikigai", "Japan", 2012)).toEqual(
         "Book Added"
       );
       expect(library.books).toHaveLength(1);
     });
-    it("should remove book if we run borrow or raise an error if book is not available", () => {
+
+
+    // Test case to check **BorrowBook** Function whenever it's **Success**
+    it("should change availibility of book whenever user borrow book", () => {
       library.addBook("1234", "Ikigai", "Japan", 2012);
-      const b = library.borrowBook("1234");
-      expect(b.title).toEqual("Ikigai");
+      const book = library.borrowBook("1234");
+      expect(book.title).toEqual("Ikigai");
       expect(library.books[0].isAvailable).toEqual(false);
     });
+
+
+    // Test case to Notify User future Availibility of Book from **borrowBook**
     it("should inform about the availability of book in future", () => {
       library.addBook("1234", "Ikigai", "Japan", 2012);
       library.borrowBook("1234", "31/8/2024");
@@ -25,6 +36,9 @@ describe('Library Managment Sytem',()=>{
         "Book is not Available Yet ,It will be Available after 31/8/2024"
       );
     });
+
+
+    // Test case to check **returnBook** Function
     it("should return the book of user and change availibility accoedingly", () => {
       library.addBook("1234", "Ikigai", "Japan", 2012);
       library.borrowBook("1234", "31/8/2024");
@@ -32,6 +46,9 @@ describe('Library Managment Sytem',()=>{
       expect(library.books[0].isAvailable).toEqual(true);
       expect(library.books[0].willBeAvailableAt).toEqual(undefined);
     });
+
+
+    // Test case to check **returnBook** Function Error
     it("should raise an error if someone is returning book that doesn't belong to library", () => {
       library.addBook("1234", "Ikigai", "Japan", 2012);
       library.borrowBook("1234", "31/8/2024");
@@ -39,6 +56,9 @@ describe('Library Managment Sytem',()=>{
         "Book don't Belong to Library"
       );
     });
+
+
+    // Test Case to check **viewBook** Function
     it("should return all books that are available in the library", () => {
       library.addBook("1234", "Ikigai", "Japan", 2012);
       library.addBook("1235", "saurashtrini rasdhar", "unknown", 2005);
